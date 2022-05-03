ab0866f9c1426fae5da38616e5500a410196b26b
saylaww <saylaww@gmail.com>
2022-05-03 21:30:15 +0500

    fdfdfdf

---
+++ src/main/java/uz/nukuslab/birinshiapp/BirinshiappApplication.java
@@ -0,0 +1,21 @@
+package uz.nukuslab.birinshiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class BirinshiappApplication {
+
+    @GetMapping("/")
+    public String index(){
+        return "TEST page";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(BirinshiappApplication.class, args);
+    }
+
+}
